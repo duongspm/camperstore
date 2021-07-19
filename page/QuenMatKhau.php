595e71aa8bfaaf9e3cf4bc223d9572d5af29ed1a
Trần Văn Dương <56970251+duongspm@users.noreply.github.com>
2021-07-19 17:39:09 +0700

    Update QuenMatKhau.php

--- page/QuenMatKhau.php
+++ page/QuenMatKhau.php
@@ -3,17 +3,6 @@
 include("../layout/header.php");
 
 ?>
-
-<div class="breadcrumbs">
-    <div class="container">
-        <div class="breadcrumbs-main">
-            <ol class="breadcrumb">
-                <li><a href="index.php">Trang chủ</a></li>
-                <li class="active">Lấy lại mật khẩu</li>
-            </ol>
-        </div>
-    </div>
-</div>
 <!--end-breadcrumbs-->
 <!--start-account-->
 <div class="account">
@@ -28,13 +17,28 @@ include("../layout/header.php");
                     <div class="address">
                         <span>Tên đăng nhập</span>
                         <input id="tendangnhap" name="tendangnhap" type="text">
-                        <span style="color: red;" id="thongbao">
-                            Hãy điền tên đăng nhập của bạn, mật khẩu sẽ được gửi qua email của bạn lúc bạn đăng ký.
-                        </span>
+                        <span>Email</span>
+                        <input id="email" name="email" type="email">
+                        <br>
+                        <hr>
+                        <span>Mặt khẩu mới</span>
+                        <input type="password" id="matkhaumoi" name="matkhaumoi" type="text">
+                        <span>Xác nhận mặt khẩu</span>
+                        <input type="password" id="matkhauxacnhan" name="matkhauxacnhan" type="text">
+                        <hr>
+                        <div class="alert alert-success">
+                            <strong>Thông báo!</strong>
+                            <span style="color: red;" id="thongbao">
+                                Bạn còn nhớ tên đăng nhập và email tạo tài khoản của mình chứ ??
+                            </span>
+                        </div>
+                        
                     </div>
                     <div class="address">
                         <input id="laymk" type="submit" value="Lấy lại mật khẩu">
-
+                    </div>
+                    <div id="formFooter">
+                        <a class="underlineHover" name="forget" href="" onclick="alert('Chắc bạn tuyệt vọng lắm mới bấm vào tôi \nGọi điện thoại tới thằng chủ dùm minh. OK?');">Hết cách !</a>
                     </div>
             </div>
             </form>
@@ -49,15 +53,18 @@ global $conn;
 if($_SERVER["REQUEST_METHOD"]=="POST")
 {
     $tendangnhap=$_POST["tendangnhap"];
-    $ktTaiKhoan="SELECT * FROM thanhvien WHERE TenDangNhap='".$tendangnhap."'";
+    $email=$_POST["email"];
+    $ktTaiKhoan="SELECT * FROM thanhvien WHERE TenDangNhap='".$tendangnhap."' and Email='".$email."'";
     $truyvankyTaiKhoan=mysqli_query($conn,$ktTaiKhoan);
 
     if(mysqli_num_rows($truyvankyTaiKhoan) > 0)
     {
-        $cot=mysqli_fetch_array($truyvankyTaiKhoan);
-
-        if(mail($cot["Email"],"Lấy lại mật khẩu website bán hàng","Xin chào ! \n Mật khẩu của bạn là: ".$cot["MatKhau"],"From: taikhoangg0@gmail.com"))
-            echo "<script> $('#thongbao').text('Lấy lại mật khẩu thành công, hãy kiểm tra email của bạn');</script>";
+        // //$cot=mysqli_fetch_array($truyvankyTaiKhoan);
+        // echo "<script> $('#thongbao').text('Đã có lỗi xảy ra');</script>";
+        $thaydoiMatKhau="UPDATE thanhvien SET MatKhau='".$_POST["matkhaumoi"]."'WHERE TenDangNhap='".$_POST["tendangnhap"]."'";
+        if(mysqli_query($conn,$thaydoiMatKhau))
+     
+            echo "<script> $('#thongbao').text('Lấy lại mật khẩu thành công');</script>";
         else
             echo "<script> $('#thongbao').text('Đã có lỗi xảy ra');</script>";
     }
@@ -72,12 +79,29 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
     $(document).ready(function(){
         $('#laymk').click(function(){
             tendangnhap=$('#tendangnhap').val();
+            matkhaumoi=$('#matkhaumoi').val();
+            matkhauxacnhan=$('#matkhauxacnhan').val();
 
             loi=0;
             if(tendangnhap=="")
             {
                 loi++;
                 $('#thongbao').text("Hãy nhập tên đăng nhập");
+            }
+            if(matkhaumoi=="")
+            {
+                loi++;
+                $('#thongbao').text("Hãy nhập mật khẩu mới của bạn");
+            }
+            if(matkhauxacnhan=="")
+            {
+                loi++;
+                $('#thongbao').text("Bạn chưa xác nhận mật khẩu");
+            }
+            if(matkhaumoi != matkhauxacnhan)
+            {
+                loi++;
+                $('#thongbao').text("Mật khẩu không trùng khớp");
             }
 
             if(loi!=0)
